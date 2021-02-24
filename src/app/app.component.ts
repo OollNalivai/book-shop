1bd852177f4a62c9c3f22c82c61e4b1e82d5a769
VladislavOoll <apahiphip@mail.ru>
2021-02-24 23:45:28 +0300

    eslinter added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'tp-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'book-shop';
+    title = 'book-shop';
+
+    products = [123, 1231234, 45345345, 4436, 55, 6, 89, 0, 2, 32, 234];
+
+    items = ['Tom', 'Bob', 'Sam', 'Bil11'];
+
+    array = [23, 34, 455, 4436, 55, 6, 89, 0, 2, 32, 234];
+
+    // condition: boolean = true;
+
+    // toggle(){
+    //   this.condition=!this.condition;
+    // }
 }
